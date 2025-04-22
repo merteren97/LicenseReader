ad3b4d1c2999d7f2fb280ad6de223417f8d56ca8
Edip Mert Eren <mert.eren@visiomex.com>
2025-04-22 14:45:24 +0300

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            txtEncryptedInput = new TextBox();
+            btnValidate = new Button();
+            label2 = new Label();
+            lblResult = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(138, 18);
+            label1.TabIndex = 0;
+            label1.Text = "License Key :";
+            // 
+            // txtEncryptedInput
+            // 
+            txtEncryptedInput.Location = new Point(156, 8);
+            txtEncryptedInput.Name = "txtEncryptedInput";
+            txtEncryptedInput.Size = new Size(322, 23);
+            txtEncryptedInput.TabIndex = 1;
+            // 
+            // btnValidate
+            // 
+            btnValidate.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnValidate.Location = new Point(377, 37);
+            btnValidate.Name = "btnValidate";
+            btnValidate.Size = new Size(101, 57);
+            btnValidate.TabIndex = 2;
+            btnValidate.Text = "Validate";
+            btnValidate.UseVisualStyleBackColor = true;
+            btnValidate.Click += btnValidate_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 18);
+            label2.TabIndex = 3;
+            label2.Text = "Result :";
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblResult.Location = new Point(106, 56);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(0, 18);
+            lblResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(490, 103);
+            Controls.Add(lblResult);
+            Controls.Add(label2);
+            Controls.Add(btnValidate);
+            Controls.Add(txtEncryptedInput);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "License Reader";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtEncryptedInput;
+        private Button btnValidate;
+        private Label label2;
+        private Label lblResult;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,71 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
 namespace LicenseReader
 {
     public partial class Form1 : Form
     {
+        private static readonly string gizliSifre = "1MertinGizliAnahtari1";
         public Form1()
         {
             InitializeComponent();
+        }
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            string encryptedText = txtEncryptedInput.Text.Trim();
+
+            try
+            {
+                string decryptedGuid = Decrypt(encryptedText, gizliSifre);
+                string systemGuid = GetMachineGuid();
+
+                if (decryptedGuid == systemGuid)
+                {
+                    lblResult.Text = "✅ Doğrulama Başarılı!";
+                }
+                else
+                {
+                    lblResult.Text = "❌ GUID uyuşmuyor.";
+                }
+            }
+            catch
+            {
+                lblResult.Text = "❌ Şifre çözülemedi veya hatalı format.";
+            }
+        }
+        private string Decrypt(string cipherText, string key)
+        {
+            byte[] keyBytes = GetFixedSizeKey(key);
+            byte[] iv = Encoding.UTF8.GetBytes("1234567890abcdef");
+
+            using Aes aes = Aes.Create();
+            aes.Key = keyBytes;
+            aes.IV = iv;
+
+            using MemoryStream ms = new(Convert.FromBase64String(cipherText));
+            using CryptoStream cs = new(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            using StreamReader sr = new(cs);
+            return sr.ReadToEnd();
+        }
+        private byte[] GetFixedSizeKey(string key)
+        {
+            using SHA256 sha = SHA256.Create();
+            return sha.ComputeHash(Encoding.UTF8.GetBytes(key));
+        }
+        private string GetMachineGuid()
+        {
+            using (var key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Cryptography"))
+            {
+                if (key != null)
+                {
+                    object value = key.GetValue("MachineGuid");
+                    if (value != null)
+                    {
+                        return value.ToString();
+                    }
+                }
+            }
+            return string.Empty;
         }
     }
 }
